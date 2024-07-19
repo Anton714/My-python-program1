e25801eb64c40230a61cd0a02859769d5d4504dd
Anton714 <an714.am@gmail.com>
2024-07-19 18:59:44 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+num_of_homework=12
+num_of_hours=1.5
+name_of_cours="Python"
+time_for_one_task=1.5/12
+print("Курс:",name_of_cours,", всего задач:",num_of_homework,", затрачено часов:",num_of_hours,", среднее время выполнения задачи:",time_for_one_task," часа.")
