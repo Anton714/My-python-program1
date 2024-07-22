a4fba165769648f75788cb030e8002f51b28b989
Anton714 <an714.am@gmail.com>
2024-07-22 15:10:06 +0500

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,10 @@
+#task 1
+my_string=input('Введите строку: ')
+print(len(my_string))
+
+#task 2
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
