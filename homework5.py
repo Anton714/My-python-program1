9acc095c99721cf6e7254809d8fe494a69ee473f
Anton714 <an714.am@gmail.com>
2024-07-22 20:44:38 +0500

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,6 @@
+immutable_var='Dell','Big',1,44,714,False
+print(immutable_var)
+# immutable_var[1]='Esc' - кортеж не изменяемый объект
+mutable_list=['Jon','Lilu',55,12345,True]
+mutable_list[1]='Lussi'
+print(mutable_list)
